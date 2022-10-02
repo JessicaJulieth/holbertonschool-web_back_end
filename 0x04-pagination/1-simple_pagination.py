5d8fef8509f43b7aa6645f522366d8b4b2cbe435
JessicaJulieth <3497@holbertonschool.com>
2022-10-01 19:18:29 -0500

    Implement a method named get_page that takes two integer

---
+++ 0x04-pagination/1-simple_pagination.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+"""
+Implement a get_hyper method that takes the same arguments
+(and defaults) as get_page and returns a dictionary containing
+the key-value pairs.
+"""
+
+import csv
+import math
+from typing import List
+
+index_range = __import__('0-simple_helper_function').index_range
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        """Initialized
+        """
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        ''' Return page of dataset. '''
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
+
+        indices = index_range(page, page_size)
+        start = indices[0]
+        end = indices[1]
+
+        try:
+            return self.dataset()[start:end]
+        except IndexError:
+            return []
