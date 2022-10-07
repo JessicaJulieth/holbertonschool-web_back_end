6e722f14f3255c8457dbd7a40c620b901b27f5b5
JessicaJulieth <3497@holbertonschool.com>
2022-10-06 21:32:01 -0500

    Advance task

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Duck typing - first element of a sequence
+"""
+from typing import Union, Sequence, Any
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """Safe the first element
+    """
+    if lst:
+        return lst[0]
+    else:
+        return None
