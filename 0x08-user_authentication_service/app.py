3bdf0f7fe775bce6f37810bf49aeaf37bd925e1d
JessicaJulieth <3497@holbertonschool.com>
2022-10-18 22:22:03 -0500

    Pycode

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -27,8 +27,8 @@ def register():
     email = request.form.get("email")
     password = request.form.get("password")
     try:
-        user = AUTH.register_user(email, password)
-        return jsonify({"email": user.email, "message": "user created"}), 200
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"}), 200
     except Exception:
         return jsonify({"message": "email already registered"}), 400
 
