c5d3d5cde3196b45b80f9ad30b68ba74f17c9b1d
JessicaJulieth <3497@holbertonschool.com>
2022-10-18 22:23:20 -0500

    Pycode

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -41,16 +41,14 @@ def login():
     Return:
         JSON payload
     """
-    email = request.form.get("email")
-    password = request.form.get("password")
-    if AUTH.valid_login(email, password):
-        session_id = AUTH.create_session(email)
-        respons = make_response({"email": email, "message": "logged in"})
-        respons.set_cookie("session_id", value=session_id, domain="0.0.0.0",
-                           secure=False)
-        return respons
-    else:
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if AUTH.valid_login(email, password) is False:
         abort(401)
+    session_id = AUTH.create_session(email)
+    respons = jsonify({'email': email, 'message': 'logged in'})
+    respons.set_cookie('session_id', session_id)
+    return respons
 
 
 @app.route('/sessions', methods=['DELETE'], strict_slashes=False)
