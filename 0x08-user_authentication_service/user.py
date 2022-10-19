42dd0cfe9f7dd808c3b4d1b8c053ad0c97308ea7
JessicaJulieth <3497@holbertonschool.com>
2022-10-18 21:38:26 -0500

    Pycode

--- 0x08-user_authentication_service/user.py
+++ 0x08-user_authentication_service/user.py
@@ -20,8 +20,7 @@ def Bienvenue() -> str:
 @app.route("/users", methods=['POST'], strict_slashes=False)
 def register() -> str:
     """
-    POST /users
-    Return: JSON payload
+    POST /users Return: JSON payload
     """
     email = request.form.get("email")
     password = request.form.get("password")
@@ -45,8 +44,8 @@ def login() -> str:
     if AUTH.valid_login(email, password):
         session_id = AUTH.create_session(email)
         respons = make_response({"email": email, "message": "logged in"})
-        respons.set_cookie("session_id", value=s_id, domain="0.0.0.0",
-                       secure=False)
+        respons.set_cookie("session_id", value=session_id,
+                           domain="0.0.0.0", secure=False)
         return respons
     else:
         abort(401)
