8f64bf1187206735a3589cd6ebd85c07074c352a
JessicaJulieth <3497@holbertonschool.com>
2022-10-18 21:58:02 -0500

    pycode

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -8,7 +8,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from typing import TypeVar, Any
+from typing import TypeVar
 from user import Base, User
 
 
