371b6a153d27f4ea4e204d327b3e9c0ce0fea93a
JessicaJulieth <3497@holbertonschool.com>
2022-11-05 23:50:21 -0500

    Add commit

--- 0x0C-MySQL_Advanced/7-average_score.sql
+++ 0x0C-MySQL_Advanced/7-average_score.sql
@@ -1,11 +1,11 @@
 --Write a SQL script that creates a stored procedure ComputeAverageScoreForUser
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
-DELIMITER //
+DELIMITER |
 CREATE PROCEDURE ComputeAverageScoreForUser
 (IN user_id INT) 
 BEGIN
    DECLARE prom FLOAT;
    SET prom = (SELECT AVG(score) FROM corrections AS newTable WHERE newTable.user_id=user_id);
    UPDATE users SET average_score=prom WHERE id=user_id;  
-END//
-DELIMITER;
+END;
+|
