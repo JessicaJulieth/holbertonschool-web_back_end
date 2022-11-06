dc642dd7a9e72521f73a692303dc3cf92ba09b84
JessicaJulieth <3497@holbertonschool.com>
2022-11-05 23:37:44 -0500

    Validate

--- 0x0C-MySQL_Advanced/5-valid_email.sql
+++ 0x0C-MySQL_Advanced/5-valid_email.sql
@@ -1,7 +1,7 @@
 --Write a SQL script that creates a trigger that resets the
 --attribute valid_email only when the email has been changed.
 DELIMITER //
-CREATE TRIGGER validate_email
+CREATE TRIGGER valid_email
     BEFORE UPDATE ON users FOR EACH ROW
     BEGIN
         IF NEW.email <> OLD.email THEN
