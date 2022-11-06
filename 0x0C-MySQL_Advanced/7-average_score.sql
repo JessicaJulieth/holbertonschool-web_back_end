2c2c11f266a65c23405136e24bb6827fcbcfc81c
JessicaJulieth <3497@holbertonschool.com>
2022-11-05 23:44:41 -0500

    Add commit

--- 0x0C-MySQL_Advanced/7-average_score.sql
+++ 0x0C-MySQL_Advanced/7-average_score.sql
@@ -3,7 +3,7 @@ DELIMITER //
 CREATE PROCEDURE ComputeAverageScoreForUser
 (IN user_id INT) 
 BEGIN
-   UPDATE users SET overall_score = 
+   UPDATE users SET average_score = 
    (SELECT AVG(score) FROM corrections WHERE corrections.user_id=user_id GROUP BY corrections.user_id )
    WHERE id=user_id;
 END//
