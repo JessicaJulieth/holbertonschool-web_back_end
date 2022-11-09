cf141e4c9a3f415d6b2ab80ac4a3470a2aea2de4
JessicaJulieth <3497@holbertonschool.com>
2022-11-09 00:21:30 -0500

    ADD

--- 0x0C-MySQL_Advanced/3-glam_rock.sql
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -1,3 +1,3 @@
---Write a SQL script that lists all bands with Glam rock as their main style, ranked by their longevity
+-- Write a SQL script that lists all bands with Glam rock as their main style, ranked by their longevity
 SELECT band_name, (IFNULL(split, 2022) -  formed) AS lifespan
 FROM metal_bands WHERE style REGEXP 'Glam rock' ORDER BY lifespan DESC;
