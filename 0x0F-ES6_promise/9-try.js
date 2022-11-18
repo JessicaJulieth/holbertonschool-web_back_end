271fcb1f5d097d26af32ab2c8f2896b4ec044918
JessicaJulieth <3497@holbertonschool.com>
2022-11-18 13:05:46 -0500

    Add

--- 0x0F-ES6_promise/9-try.js
+++ 0x0F-ES6_promise/9-try.js
@@ -1,12 +1,15 @@
 export default function guardrail(mathFunction) {
-    const queue = [];
-    let val = 0;
-    try {
-      val = mathFunction();
-    } catch (e) {
-      val = `${e.name}: ${e.message}`;
-    }
-    queue.push(val);
-    queue.push('Guardrail was processed');
-    return queue;
+  const queue = [];
+  let value;
+
+  try {
+    value = mathFunction();
+  } catch (e) {
+    value = e.toString();
   }
+
+  queue.push(value);
+  queue.push('Guardrail was processed');
+
+  return queue;
+}
