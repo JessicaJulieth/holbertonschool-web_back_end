9d3cb07c37c77f3d1f18026041c81acddf62219a
JessicaJulieth <3497@holbertonschool.com>
2022-11-18 19:57:03 -0500

    Add

--- 0x0D-NoSQL/11-schools_by_topic.py
+++ 0x0D-NoSQL/11-schools_by_topic.py
@@ -6,6 +6,4 @@ def schools_by_topic(mongo_collection, topic):
     """
     Python function that returns the list of school having a specific topic
     """
-    documents = mongo_collection.find({"topics": topic})
-    list_docs = [i for i in documents]
-    return list_docs
+    return mongo_collection.find({"topics": topic})
