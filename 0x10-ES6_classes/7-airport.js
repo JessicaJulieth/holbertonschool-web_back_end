24e289564dc0135be706f7b70c076ea9a14bf27e
JessicaJulieth <3497@holbertonschool.com>
2022-11-20 20:26:34 -0500

    corrections

--- 0x10-ES6_classes/7-airport.js
+++ 0x10-ES6_classes/7-airport.js
@@ -1,12 +1,10 @@
-const util = require('util');
-
 export default class Airport {
   constructor(name, code) {
-    if (typeof name === 'string') this._name = name;
-    if (typeof code === 'string') this._code = code;
+    this._name = name;
+    this._code = code;
   }
 
-  [util.inspect.custom]() {
-    return `${this.constructor.name} [${this._code}] { _name: '${this._name}', _code: '${this._code}' }`;
+  toString() {
+    return `[${typeof this} ${this._code}]`;
   }
 }
