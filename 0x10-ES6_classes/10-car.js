43586399173db089de4262893f4b0b7a599d0607
JessicaJulieth <3497@holbertonschool.com>
2022-11-20 19:48:29 -0500

    Last task

---
+++ 0x10-ES6_classes/10-car.js
@@ -0,0 +1,17 @@
+export default class Car {
+    constructor(brand, motor, color) {
+      this._brand = brand;
+      this._motor = motor;
+      this._color = color;
+    }
+  
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    const c = this.constructor;
+    return new c[Symbol.species](this._brand, this._motor, this._color);
+  }
+}
