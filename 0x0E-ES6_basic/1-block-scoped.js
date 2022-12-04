9a32d145eefd00ad1dd161f62021863f8aed2680
JessicaJulieth <3497@holbertonschool.com>
2022-12-04 18:45:39 -0500

    Add

--- 0x0E-ES6_basic/1-block-scoped.js
+++ 0x0E-ES6_basic/1-block-scoped.js
@@ -1,8 +1,9 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
+    /*Block*/
     task = true;
     task2 = false;
   }
